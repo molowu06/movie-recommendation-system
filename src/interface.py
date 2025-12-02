f4e3d7f25cadd0c392064bec821f4e661f8001f7
StephanieRojasGonzalez <rojasgonzalez.s@northeastern.edu>
2025-12-01 19:55:51 -0500

    some changes

--- src/interface.py
+++ src/interface.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import time
+from data_loader import *
 
 st.title("Movie Reccomendation Software")
 st.write("Creators: Mimo Molowu, Stephanie Rojas Gonzales, Bhavana Kakumanu")
@@ -20,6 +21,13 @@ st.write("### You selected:")
 for genre in chosen_genres:
     st.markdown(f"- :orange[{genre}]")
 
+
+movies_df = load_movies("data/ml-latest-small/movies.csv")
+ratings_df = load_ratings("data/ml-latest-small/ratings.csv")
+avg_ratings_df = avg_ratings(ratings_df)
+merge_df = merge_movies_ratings(movies_df, avg_ratings_df)
+
+print(merge_df)
 
 
 # dates (newest to oldest)
